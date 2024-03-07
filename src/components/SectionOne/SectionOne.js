bffc51781807e3bbc0e849f42b1372e13bb933ef
syed nasar <84076077+nasarlalu@users.noreply.github.com>
2024-03-07 16:50:25 +0530

    download added

--- src/components/SectionOne/SectionOne.js
+++ src/components/SectionOne/SectionOne.js
@@ -1,6 +1,8 @@
 import styles from './style.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 import banner from '@/public/assets/images/home_banner.jpg'
+import { AiOutlineDownload } from "react-icons/ai";
 export default function SectionOne() {
     return (
         <section className={styles.section_one}>
@@ -20,6 +22,14 @@ export default function SectionOne() {
                             <h6>Front End Developer</h6>
                             {/* <p>Weaving innovation and aesthetics into every line of code – crafting immersive and visually captivating web experiences for the digital landscape."</p> */}
                             <p>The wizard behind the pixels, concocting code magic for sleek and funky web vibes – where innovation dances with minimalistic coolness, creating digital experiences that pop!</p>
+
+                            <Link href='/assets/syed_nasar_resume.pdf' className={styles.download_btn}>
+                                <button>
+                                    <span>Download CV</span>
+                                    <span><AiOutlineDownload /></span>
+                                </button>
+                            </Link>
+
                         </div>
                     </div>
                 </div>
