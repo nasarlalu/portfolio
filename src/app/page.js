08326bf1982a2cab0f1f957a9982dc06433d0471
syed nasar <84076077+nasarlalu@users.noreply.github.com>
2024-03-13 00:24:26 +0530

    service section update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SectionOne from "@/src/components/SectionOne/SectionOne"
-import SmoothScrolling from "@/src/components/SmoothScrolling/SmoothScrolling"
+import Services from "@/src/components/Services/Services"
 
 export default function Home() {
   return (
     <>
       <SectionOne />
+      <Services />
     </>
   );
 }
